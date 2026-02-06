74c828e3bd6bc992b7abb788162da4462eb55a5d
Ankit kumar gupta <ak0199374@gmail.com>
2026-02-06 21:09:40 +0530

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,12 @@
+# 1.Integers-->..-2,-1,0,1,2,3....
+# 2.Floating point numbers-->Decimal values like->2.3,3.55...
+# 3.strings-> using double quotes or single.
+# 4.booleans
+# 5.None
+a=1 # a is an integer
+b=3.22 # b is a float point number
+c="ANKIT" #c is a string
+d= False # d is  a boolean
+e= None #   e is a none data type
+
+
