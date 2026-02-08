b73d6266158f5f0057f7abc1a64e0be0d53c8e9b
Ankit kumar gupta <ak0199374@gmail.com>
2026-02-08 15:04:38 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,27 @@
+# 1.Arithmetic operators->used to perform mathematical operations. like->.+,-.*,/,//,%
+#2.Assignment operators-> =(equals too),+=,-=,*=,/=..
+#3.Comparison operators--> ==,>,>=,<,<=.!= etc..
+#4.Logical operators--> and ,or ,not..
+# ARITHMETIC OP:--
+a=3
+b=5
+print(a+b)  
+
+#ASSIGNMENT OP:
+c=6
+c+=3 #Increment the values of b by 3 and then assign it to b
+c-=3 #Decrement
+print(c)
+
+#Comparision op-->it returns always a booleans values i.e ->>True or False..
+d=5<4
+print(d)
+print(5==5)
+print(5!=9)
+print(5>=9)
+
+#   Logical operators
+e=True or False
+print(e)
+
+print(not(True))
