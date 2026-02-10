a9f2138c10788fb1e0ee4d9b86143e4151c88aba
Ankit kumar gupta <ak0199374@gmail.com>
2026-02-10 10:53:53 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,6 @@
+#Variable is a container which is used to stores data.
+a=1 # a is a variable here..and 1 is the data that stores in a.
+b=2
+print(a+b)
+
+b="Ankit"#b is a variable that stores a string value i..e ANKIT
