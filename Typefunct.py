5fa261c737092923bc7e4d08bb01ae332d27a150
Ankit kumar gupta <ak0199374@gmail.com>
2026-02-10 10:53:39 +0530

    Add files via upload

---
+++ Typefunct.py
@@ -0,0 +1,16 @@
+#Type funct-> it gives the type of the data type
+a=3
+print(type(a))
+b="char"
+print(type(b))
+c=True
+print(type(c))
+d=None
+print(type(d))
+e=9.33
+print(type(e))
+
+
+a="23"
+b=int(a)
+print(type(b))
