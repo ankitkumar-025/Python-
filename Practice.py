fd53b944d8464b7c27683237ee6518ecc0404015
Ankit kumar gupta <ak0199374@gmail.com>
2026-02-13 00:19:56 +0530

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,67 @@
+# d={
+#     "kela":"banana",
+#     "billi":"cat",
+#     "angoor" :"grapes"
+# }
+# word=input("Enter the word you meaning of:")
+# print(d[word])
+
+#  Q2
+# s=set()
+# numbers=int(input("Enter number:"))
+# s.add(numbers)
+# numbers=int(input("Enter number:"))
+# s.add(numbers)
+# numbers=int(input("Enter number:"))
+# s.add(numbers)
+# numbers=int(input("Enter number:"))
+# s.add(numbers)
+# numbers=int(input("Enter number:"))
+# s.add(numbers)
+# numbers=int(input("Enter number:"))
+# s.add(numbers)
+# numbers=int(input("Enter number:"))
+# s.add(numbers)
+# numbers=int(input("Enter number:"))
+# s.add(numbers)
+# print(s)
+
+#   Q3
+# s=set()
+# s.add(20)
+# s.add(20.0)# 20==20.0
+# s.add('20')
+# print(len(s))
+# print(s)
+
+#Q4
+
+# d={}
+# names=input("Enter names:")
+# lang=input("Enter a language:")
+# d.update({names:lang})
+
+# names=input("Enter names:")
+# lang=input("Enter a language:")
+# d.update({names:lang})
+
+
+# names=input("Enter names:")
+# lang=input("Enter a language:")
+# d.update({names:lang})
+
+
+# names=input("Enter names:")
+# lang=input("Enter a language:")
+# d.update({names:lang})
+# print(d)
+
+# sets are immutable and hashable: -> cannot change
+# lists are not added to set..
+
+
+
+
+    
+
+
