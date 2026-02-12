6e612403f083c25516b623958d00bc7a549b8ede
Ankit kumar gupta <ak0199374@gmail.com>
2026-02-12 12:30:44 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,10 @@
+friends=["ankit","nayan",5,"monkey","grapes"]
+print(friends)
+print(friends[0])
+friends[0]="Ayush"# List are mutable which means it can be changed or update later...
+print(friends)
+print(friends[2])
+#   Indexing are same as String..
+print(friends[:4])
+friends.append("Ankit")
+print(friends)
