4dbd2483c7b049dafccd369fadb8a6febbf21cb2
Ankit kumar gupta <ak0199374@gmail.com>
2026-02-12 12:29:32 +0530

    Add files via upload

--- Problem2.py
+++ Problem2.py
@@ -1,9 +1,6 @@
-#Using External modules--like here it is pyttsx3
-import pyttsx3
-engine = pyttsx3.init()
-
-# For Mac, If you face error related to "pyobjc" when running the `init()` method :
-# Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
-
-engine.say("My name is Ankit kumar gupta")
-engine.runAndWait()
+letter='''
+Dear <|Name|>,
+You are selected!
+<|Date|>
+'''
+print(letter.replace("<|Name|>","Ankit").replace("<|Date|>","25 November 2023"))
