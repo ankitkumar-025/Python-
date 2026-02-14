6f30fbf80f5663e32e11c20586e5cf9018e75374
Ankit kumar gupta <ak0199374@gmail.com>
2026-02-14 11:46:11 +0530

    Add files via upload

---
+++ ConditionalSt.py
@@ -0,0 +1,21 @@
+# Multiple if statement:
+a=int(input("Enter your age:"))
+
+# if statement 1:
+if (a%2==0):
+    print("a is even")
+# end of if st :1
+# 
+# if st 2:
+if (a>=18) :
+    print()
+elif(a>0):
+    print()
+
+elif (a<0):
+    print()
+
+else:
+    print("")
+
+# end of if stt 2   
