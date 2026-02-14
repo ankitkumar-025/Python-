c971faea9c412d5f03584f437f5fe5e85a58f0df
Ankit kumar gupta <ak0199374@gmail.com>
2026-02-14 11:46:27 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,28 @@
+# num=int(input("Enter a number:"))
+
+# for i in range(1,10):
+#     print(f"{num} x {i} = {num * i}")
+     
+
+  
+           
+
+# i=1
+# while(i<=10):
+#     print(num*i,end=" ")
+#     i+=1
+
+l=["ankit","sachin","rahul"]
+for i in l:
+    print("Hello",i)
+
+# Prime or not
+n=int(input())
+for i in range(2,n):
+    if n%1==0 and n%n==0:
+        print(f"Number is prime",{n})
+        break
+    else:
+
+        
+        print("Number is not prime")    
