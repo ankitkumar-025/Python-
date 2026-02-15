2b815d8d42c7375aa06cb6af3cd705309a21629d
Ankit kumar gupta <ak0199374@gmail.com>
2026-02-15 19:12:31 +0530

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,49 @@
+# def name(): #Function Defination
+#     print("Hello")
+
+# name() #Function call
+# name()
+# name()
+# name()
+# name()
+
+# def avg():
+#     a=int(input("Enter a number: "))
+#     b=int(input("Enter a number: "))
+#     c=int(input("Enter a number: "))
+#     avg=(a+b+c)/3
+#     print(avg)
+# avg() 
+# print("Thanks")   
+# avg()    
+
+# avg()    
+
+# avg()    
+
+# Examples of Functions With Arguments:
+# def goodDay(name,surname):
+#     print("Good Day," + name +" "+ surname)
+
+# goodDay("Ankit" , "Gupta")
+
+# goodDay("Ankita" , "Gupta")
+
+# goodDay("Ankitii" , "Gupta")
+
+#Default Parameter Value
+# Surname= "Gupta"..
+# def goodDay(name,surname="Gupta"):
+#     print("Good Day," + name +" "+ surname)
+
+# goodDay("Ankit","Kumar")
+
+# goodDay("Ankit")
+# #Only 1 name argument passed ..not the surname value bec it already initialized with the fixed value or it will print the default value i.e,, "Gupta"
+# def name(name="ankit"):
+#     print("Hello",name)
+
+# name()   
+# name("YASH") 
+
+
