8f1f54a8965d135b5dffbd678db2f8db1e0fafe4
Ankit kumar gupta <ak0199374@gmail.com>
2026-03-01 09:00:16 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,8 @@
+# Inches to cm
+def inches_To_Cm(inch):
+    return inch * 2.54  #formula to convert inches into cm.
+
+# print(inches_To_Cm(6))
+
+n=int(input("Enter value in inches:"))
+print(f"The correspomding values in cm is {inches_To_Cm(n)} ")
