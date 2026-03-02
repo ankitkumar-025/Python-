8555518cf72dbeddfd5ad280b6eb48c7b9fc1972
Ankit kumar gupta <ak0199374@gmail.com>
2026-03-02 10:16:55 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,8 @@
+def mulTable(n):
+    # n=int(input("Enter a number:"))
+    for i in range(1,11):
+        print(f"{n} * {i} = {n * i}")
+    print()
+n=int(input("Enter a number: "))
+mulTable(n)   
+   
