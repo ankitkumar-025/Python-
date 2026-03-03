897b45007776e433c8a59507b343902bd892654c
Ankit kumar gupta <ak0199374@gmail.com>
2026-03-03 11:07:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+    # Creating Nodes:
+node1=Node(10)
+node2=Node(20)
+node3=Node(30)
+node4=Node(40)
+
+    #Connect nodes to form a linked list:
+node1.next=node2
+node2.next=node3
+node3.next=node4
+
+    #Printing the linked list:
+current=node1
+while current is not None:
+    print(current.data,end="->")
+    current=current.next
+print("None")    
+    
+     
+
+    
