cc51f83f9a5e2e96772a32fd0db3a46b401ee284
Ankit kumar gupta <ak0199374@gmail.com>
2026-03-05 11:22:02 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,24 @@
+# marks=[]
+# f1=int(input("Enter marks:"))
+# marks.append(f1)
+# f2=int(input("Enter marks:"))
+# marks.append(f2)
+# f3=int(input("Enter marks:"))
+# marks.append(f3)
+# f4=int(input("Enter marks:"))
+# marks.append(f4)
+# f5=int(input("Enter marks:"))
+# marks.append(f5)
+# f6=int(input("Enter marks:"))
+# marks.append(f6)
+
+# marks.sort()
+# print(marks)
+
+# # Tuple is immutable.. # you cant change or update further:
+# list=[1,2,34,56,3]
+# print(sum(list))
+
+# Write a program to count the number of zeroes in a tuple:
+t=(1,2,3,4,0,5,0,0,0)
+print(t.count(0)) #for counting ..
