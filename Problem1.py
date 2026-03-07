c9ba87bf137d975d6e64d2bf4c709c3c04e4840d
Ankit kumar gupta <ak0199374@gmail.com>
2026-03-07 10:27:17 +0530

    Add files via upload

--- Problem1.py
+++ Problem1.py
@@ -1,2 +1,10 @@
-name=(input("Enter Your Name:"))
-print(f"GOood Afternoon {name}")
+class Programmer:
+    company="Microsoft"
+    def __init__(self,name,salary,pin):
+        self.name=name
+        self.salary=salary
+        self.pin=pin
+
+p=Programmer("Ankit",1200000,834005)
+print(p.name,p.salary,p.pin,p.company)        
+    
