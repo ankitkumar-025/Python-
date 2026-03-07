2269d864d10855160bf8c6b07a2a180f1f032952
Ankit kumar gupta <ak0199374@gmail.com>
2026-03-07 10:27:34 +0530

    Add files via upload

--- Problem2.py
+++ Problem2.py
@@ -1,6 +1,25 @@
-letter='''
-Dear <|Name|>,
-You are selected!
-<|Date|>
-'''
-print(letter.replace("<|Name|>","Ankit").replace("<|Date|>","25 November 2023"))
+class calculator:
+    def __init__(self,n):
+        self.n=n
+
+    def square(self):
+        print(f"The sqaure is {self.n*self.n}")
+
+    def cube(self):
+        print(f"The cube is {self.n*self.n*self.n}") 
+
+    
+    def squareroot(self):
+        print(f"The squareroot is {self.n**1/2}")    
+       
+    @staticmethod
+    def hello():
+        print("Hello There!!")   
+
+a=calculator(9)
+a.hello()
+a.square()   
+a.cube()  
+a.squareroot()  
+
+    
