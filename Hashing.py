0cac1520b6ab295205ab0fb6dc9882d330fc3398
Ankit kumar gupta <ak0199374@gmail.com>
2026-03-11 21:33:22 +0530

    Add files via upload

---
+++ Hashing.py
@@ -0,0 +1,16 @@
+# Introduction to Hashing 
+n=[5,3,2,2,1,5,5,7,5,10]
+m=[10,111,1,9,5,67,2]
+result=[]
+hash_list=[0]*11
+for num in n:
+    hash_list[num]+=1
+for num in m:
+    if num<1 or num>10:
+        print(0)
+    else:
+        print(hash_list[num])
+        result.append(hash_list[num])   
+print(result)     
+
+
