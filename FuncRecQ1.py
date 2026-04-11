3703a629a2c9d644f5c08d4e225fa8cc67f9e528
Ankit kumar gupta <ak0199374@gmail.com>
2026-04-11 20:28:08 +0530

    Add files via upload

---
+++ FuncRecQ1.py
@@ -0,0 +1,9 @@
+# Factorial of a num ber Using Recurrsion
+def func(N):
+    if N==1 or N==1: #Base condn
+        return 1
+    return N * func(N-1) #Flow
+print(func(5))
+# N=Number..
+# TC=O(N)
+# SC=O(N)=>Stack Space
